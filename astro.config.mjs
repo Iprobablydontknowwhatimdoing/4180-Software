cd165bee3d4992788790f308827ab761e4d9cd94
iprobablydontknowwhatimdoing <octact82@gmail.com>
2026-03-23 21:48:16 -0700

    Changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightGiscus from 'starlight-giscus';
 
-// https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
+			plugins: [
+				starlightGiscus({
+					repo: 'Iprobablydontknowwhatimdoing/4180-Software',
+					repoId: 'R_kgDORvCEAQ',
+					category: 'On Pages',
+					categoryId: 'DIC_kwDORvCEAc4C5I8M',
+					mapping: 'title',
+					reactions: true,
+					inputPosition: 'top',
+				})
+			]
 		}),
 	],
 });
---
+++ src/content/docs/index.md
@@ -0,0 +1,20 @@
+---
+title: this is the main index
+description: heres an index!
+---
+
+# This is the main index
+
+Lets see how well this does
+
+- Testing
+
+* Testing
+
+1. Testing
+123. Testing
+2. Testing
+
+```text
+testing
+```
